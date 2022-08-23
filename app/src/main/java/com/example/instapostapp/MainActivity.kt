c9ad7fd5877c49690a1219dbcf1c23c033faa3e2
vako03 <vako.mekhashishvili@gmail.com>
2022-08-23 13:57:11 +0400

    added second activity

--- app/src/main/java/com/example/instapostapp/MainActivity.kt
+++ app/src/main/java/com/example/instapostapp/MainActivity.kt
@@ -1,5 +1,6 @@
  package com.example.instapostapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageView
@@ -18,7 +19,16 @@ import de.hdodenhof.circleimageview.CircleImageView
         likeImage.setOnClickListener {
             it.isSelected=!it.isSelected
         }
-        Glide.with(this).load("https://images.theconversation.com/files/304864/original/file-20191203-67028-qfiw3k.jpeg?ixlib=rb-1.1.0&rect=638%2C2%2C795%2C745&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip").into(profileImage)
-        Glide.with(this).load("https://youimg1.tripcdn.com/target/100c0z000000nbuj7D809_D_1180_558.jpg?proc=source%2Ftrip").into(postImage)
+
+            profileImage.setOnClickListener{
+                val intent = Intent(this,SecondActivity::class.java)
+                startActivity(intent)
+
+        }
+
+        Glide.with(this)
+            .load("https://images.theconversation.com/files/304864/original/file-20191203-67028-qfiw3k.jpeg?ixlib=rb-1.1.0&rect=638%2C2%2C795%2C745&q=20&auto=format&w=320&fit=clip&dpr=2&usm=12&cs=strip").into(profileImage)
+        Glide.with(this)
+            .load("https://youimg1.tripcdn.com/target/100c0z000000nbuj7D809_D_1180_558.jpg?proc=source%2Ftrip").into(postImage)
     }
 }
